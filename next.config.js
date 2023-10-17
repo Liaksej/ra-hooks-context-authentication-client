0ff1af21356b6b4cab78c753ce57d723f7347bd1
Liaksej <tarusov.liaksej@gmail.com>
2023-10-17 22:16:34 +0200

    Update README and refactored configurations for production deployment
    The initial README was replaced with project-specific information and a build status badge. An exclusion rule was added to .gitignore for better project structure. Adjustments were made in next.config.js to set a dynamic base path for better deployment usability. Unnecessary styles were removed from global.css as they were not contributing to the functionality or appearance of the application. These changes were made to ensure seamless production deployment and improve readability of the README.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/ra-hooks-context-authentication-client"
+      : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
